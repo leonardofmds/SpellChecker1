ec3110340fb2e56da38b61d8a9d77831ab29b16e
leofe <leofe@DESKTOP-TQU8GU8.Home>
2016-11-25 00:07:00 -0200

    Implementação do calculo de distância de levenshtein e do leitor do dicionário.

--- src/main/java/pm/SpellCheckerLeonardo/DictionaryReader.java
+++ src/main/java/pm/SpellCheckerLeonardo/DictionaryReader.java
@@ -1,5 +1,13 @@
 package pm.SpellCheckerLeonardo;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
+
 public class DictionaryReader {
 
 	public BurkhardKellerTree loadFromFile(String string, IDistanceCalculator calculator) 
@@ -8,10 +16,29 @@ public class DictionaryReader {
 		return null;
 	}
 		
-	public BurkhardKellerTree loadFromFile(String string) 
+	public ArrayList<String> loadFromFile(String string) 
 	{
-			// TODO Auto-generated method stub
-			return null;			
+		ArrayList<String> palavras = new ArrayList<String>();
+
+		try {
+			ZipFile zf = new ZipFile(string);
+
+			ZipEntry entry = zf.entries().nextElement();
+
+			InputStream inputStream = zf.getInputStream(entry);
+			InputStreamReader isr = new InputStreamReader(inputStream);
+			BufferedReader buffer = new BufferedReader(isr);
+			String linha;
+			while ((linha = buffer.readLine()) != null) {
+				palavras.add(linha);
+			}
+			zf.close();
+		} catch (IOException e) {
+
+			e.printStackTrace();
+		}
+
+		return palavras;
 	}
 
 }
